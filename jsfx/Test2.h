5a6dd41d8ca67ae9e6727deda26f060a5b1574ce
盛嘉炜 <shengjiawei@sjw.local>
2015-09-19 14:33:24 +0800

    debug

---
+++ jsfx/Test2.h
@@ -0,0 +1,13 @@
+//
+//  Test2.h
+//  jsfx
+//
+//  Created by 盛嘉炜 on 15/9/19.
+//  Copyright (c) 2015年 盛嘉炜. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test2 : NSObject
+
+@end
---
+++ jsfx/Test2.m
@@ -0,0 +1,13 @@
+//
+//  Test2.m
+//  jsfx
+//
+//  Created by 盛嘉炜 on 15/9/19.
+//  Copyright (c) 2015年 盛嘉炜. All rights reserved.
+//
+
+#import "Test2.h"
+
+@implementation Test2
+
+@end
